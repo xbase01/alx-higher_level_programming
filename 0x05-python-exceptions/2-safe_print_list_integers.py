a1e554f9f5f74c57741ddab0d80d0c790942d3f0
Jude <judo4christ@gmail.com>
2023-03-28 03:23:37 +0000

    safe print

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    i = count = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i], end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+        finally:
+            i += 1
+    print()
+    return count
