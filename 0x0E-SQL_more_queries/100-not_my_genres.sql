8151fc949a6129daf425d63e693f385ef0849ab1
Jude <judo4christ@gmail.com>
2023-06-11 04:16:00 +0000

    advance

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,10 +1,17 @@
--- Lists all genres of the database hbtn_0d_tvshows that are not linked
--- to the show Dexter, sorted in ascending order by genre name.
-SELECT DISTINCT g.`name`
-FROM `tv_genres` AS g
-INNER JOIN `tv_show_genres` AS s
-ON g.`id` = s.`genre_id`
-LEFT JOIN `tv_shows` AS t
-ON s.`show_id` = t.`id`
-WHERE t.`title` <> 'Dexter' OR t.`title` IS NULL
-ORDER BY g.`name` ASC;
+-- Retrieves a list of all genres that are not linked to the show Dexter.
+-- The tv_shows table contains only one record with a title of "Dexter" (the id may vary).
+-- Each record will display the name of the genre from the tv_genres table.
+-- The results will be sorted in ascending order by the genre name.
+-- A maximum of two SELECT statements can be used.
+-- The database name will be provided as an argument in the mysql command.
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.id NOT IN
+(SELECT tv_genres.id
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter")
+ORDER BY tv_genres.name;
--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,10 +1,15 @@
--- Lists all comedy shows in the database hbtn_0d_tvshows.
--- Records are ordered by descending show title.
-SELECT t.`title`
-FROM `tv_shows` AS t
-INNER JOIN `tv_show_genres` AS s
-ON t.`id` = s.`show_id`
-INNER JOIN `tv_genres` AS g
-ON g.`id` = s.`genre_id`
-WHERE g.`name` = 'Comedy'
-ORDER BY t.`title` DESC;
+-- Retrieves a list of all Comedy shows in the hbtn_0d_tvshows database.
+-- The tv_genres table contains only one record with a genre name of "Comedy" (the id may vary).
+-- Each record will display the title of the tv show.
+-- The results will be sorted in ascending order by the show title.
+-- Only one SELECT statement is allowed.
+-- The database name will be provided as an argument in the mysql command.
+
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
