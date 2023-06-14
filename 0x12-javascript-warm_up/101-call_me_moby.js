25b87b99cefa37b4e214d45a680622faac30a8d8
Jude <judo4christ@gmail.com>
2023-06-14 16:48:12 +0000

    corrected

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 // Executes theFunction 'x' number of times
 
-
 exports.callMeMoby = function (x, theFunction) {
-    while (x-- > 0) {
-	theFunction();
-    }
+  while (x-- > 0) {
+    theFunction();
+  }
 };
