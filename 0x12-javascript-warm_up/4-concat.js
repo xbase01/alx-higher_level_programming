ed2dffbefee5effd463c81b5ea9eba5dec281ad0
Jude <judo4christ@gmail.com>
2023-06-14 15:03:15 +0000

    concatenate

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+if (arg1 && arg2) {
+  console.log(arg1 + ' is ' + arg2);
+} else {
+  console.log('No argument');
+}
