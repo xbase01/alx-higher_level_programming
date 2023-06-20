4cbcbaa5751e9ff7d248f3ccbefee68f624924d6
Jude <judo4christ@gmail.com>
2023-06-20 03:07:40 +0000

    mfilter

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""
+This script accepts an argument and retrieves
+all records from the hbtn_0e_0_usa database
+where the name field matches the provided argument.
+The matching values are then displayed.
