4112a6d55c0bd86c84be735be0082e825128c062
Jude <judo4christ@gmail.com>
2023-06-20 03:47:51 +0000

    safe filter

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     """
 
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-            passwd=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,3 +4,26 @@ This script accepts an argument and retrieves
 all records from the hbtn_0e_0_usa database
 where the name field matches the provided argument.
 The matching values are then displayed.
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                         WHERE name LIKE BINARY '{}' \
+                         ORDER BY states.id ASC".format(argv[4]))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
